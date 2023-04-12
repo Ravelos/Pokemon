30443fb5cb5608c97b244d768ab1ec0aae4909ee
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-11 22:33:11 -0300

    Creates an individual file for the api requests

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,10 @@
 const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-const pokemonList = document.getElementById('pokemonList');
+const pokemonList = document.getElementById("pokemonList");
 
 function convertPokemonToLi(pokemon) {
   return `
-    <ol class="pokemons">
         <li class="pokemon">
             <span class="number">#001</span>                
             <span class="name">${pokemon.name}</span>
@@ -17,19 +16,14 @@ function convertPokemonToLi(pokemon) {
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
             </div>
         </li>
-    </ol>
 `;
 }
 
-fetch(url)
-  .then((response) => response.json())
-  .then((responseBody) => responseBody.results)
-  .then((pokemons) => {
-    for(let i=0; i< pokemons.length; i++){
-        const pokemon = pokemons[i];
-        pokemonList.innerHTML += convertPokemonToLi(pokemon);
-    }
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+pokeApi.getPokemons().then((pokemons) => {
+    const listItem = []
+  for (let i = 0; i < pokemons.length; i++) {
+    const pokemon = pokemons[i];
+    listItem.push(convertPokemonToLi(pokemon));
+  }
+  console.log(listItem)
+});
