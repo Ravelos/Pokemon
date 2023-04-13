975484891607eacc72a9467e8a89cc526229c5df
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-13 16:52:21 -0300

    Handles different requests at the same time

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,23 @@
-
-
 const pokemonList = document.getElementById("pokemonList");
+
+function convertPokemonTypeToLi(pokemonTypes) {
+  return pokemonTypes.map(
+    (typeSlot) => `<li class="type">${typeSlot.type.name}</li>`
+  );
+}
 
 function convertPokemonToLi(pokemon) {
   return `
         <li class="pokemon">
-            <span class="number">#001</span>                
+            <span class="number">#${pokemon.order}</span>                
             <span class="name">${pokemon.name}</span>
             <div class="detail">
             <ol class="types">
-                <li class="type">Grass</li>
-                <li class="type">Poison</li>
+                ${convertPokemonTypeToLi(pokemon.types).join("")}
             </ol>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            <img src="${
+              pokemon.sprites.other.dream_world.front_default
+            }" alt="${pokemon.name}">
             </div>
         </li>
 `;
