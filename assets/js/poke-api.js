b9c7af718e7b0aadd17f55f10abc86f0d4cce542
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-13 16:52:32 -0300

    Handles different requests at the same time

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,22 +1,19 @@
+
 const pokeApi = {};
 
 const offset = 0;
-const limit = 10;
+const limit = 5;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+pokeApi.getPokemonsDetails = (pokemon) => {
+  return fetch(pokemon.url).then((response) => response.json())
+}
+
 pokeApi.getPokemons = () => {
-  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
   return fetch(url)
     .then((response) => response.json())
     .then((responseBody) => responseBody.results)
-    .catch((error) => console.log(error));
-};
-
-Promise.all([
-  fetch("https://pokeapi.co/api/v2/pokemon/1"),
-  fetch("https://pokeapi.co/api/v2/pokemon/2"),
-  fetch("https://pokeapi.co/api/v2/pokemon/3"),
-  fetch("https://pokeapi.co/api/v2/pokemon/4")
-]).then((results) => {
-  console.log(results);
-});
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetails))
+    .then((detailsRequests) => Promise.all(detailsRequests))
+    .then((pokemonsDetails) => pokemonsDetails);
+}
