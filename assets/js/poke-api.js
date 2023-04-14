5e1da2b19a41dd6baed97d7cfeb68c0cfe82b6fb
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-13 22:10:48 -0300

    Creates our model

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,13 +1,30 @@
-
 const pokeApi = {};
 
 const offset = 0;
 const limit = 5;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-pokeApi.getPokemonsDetails = (pokemon) => {
-  return fetch(pokemon.url).then((response) => response.json())
+function convertPokemonApiDetailsToPokemon(pokemonDetail) {
+  const pokemon = new Pokemon();
+  pokemon.number = pokemonDetail.order;
+  pokemon.name = pokemonDetail.name;
+
+  const types = pokemonDetail.types.map((typeSlot) => typeSlot.type.name);
+  const [type] = types;
+
+  pokemon.types = types;
+  pokemon.type = type;
+
+  pokemon.photo = pokemonDetail.sprites.other.dream_world.front_default;
+
+  return pokemon;
 }
+
+pokeApi.getPokemonsDetails = (pokemon) => {
+  return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokemonApiDetailsToPokemon)
+};
 
 pokeApi.getPokemons = () => {
   return fetch(url)
@@ -16,4 +33,4 @@ pokeApi.getPokemons = () => {
     .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetails))
     .then((detailsRequests) => Promise.all(detailsRequests))
     .then((pokemonsDetails) => pokemonsDetails);
-}
+};
