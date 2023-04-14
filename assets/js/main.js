866ea09c363fcd6cf9d480fcd05904e7e4802fef
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-13 22:10:14 -0300

    Removes unused function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,17 @@
 const pokemonList = document.getElementById("pokemonList");
 
-function convertPokemonTypeToLi(pokemonTypes) {
-  return pokemonTypes.map(
-    (typeSlot) => `<li class="type">${typeSlot.type.name}</li>`
-  );
-}
 
 function convertPokemonToLi(pokemon) {
   return `
         <li class="pokemon">
-            <span class="number">#${pokemon.order}</span>                
+            <span class="number">#${pokemon.number}</span>                
             <span class="name">${pokemon.name}</span>
             <div class="detail">
             <ol class="types">
-                ${convertPokemonTypeToLi(pokemon.types).join("")}
+                ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
             </ol>
             <img src="${
-              pokemon.sprites.other.dream_world.front_default
+              pokemon.photo
             }" alt="${pokemon.name}">
             </div>
         </li>
