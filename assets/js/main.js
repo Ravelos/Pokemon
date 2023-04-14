3606253f7ef2c39bee22d90dd54b860935d05c42
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-14 17:10:36 -0300

    Includes the class according to type

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,16 @@ const pokemonList = document.getElementById("pokemonList");
 
 function convertPokemonToLi(pokemon) {
   return `
-        <li class="pokemon">
+        <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>                
             <span class="name">${pokemon.name}</span>
             <div class="detail">
             <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                ${pokemon.types
+                  .map((type) => `<li class="type ${type}">${type}</li>`)
+                  .join("")}
             </ol>
-            <img src="${
-              pokemon.photo
-            }" alt="${pokemon.name}">
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
         </li>
 `;
