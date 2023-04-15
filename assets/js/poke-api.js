eface413f1662ffa6e915e4ab6e654fc78e82c33
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-04-14 21:15:26 -0300

    Refactors the code

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,5 @@
 const pokeApi = {};
 
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
 function convertPokemonApiDetailsToPokemon(pokemonDetail) {
   const pokemon = new Pokemon();
   pokemon.number = pokemonDetail.order;
@@ -26,7 +22,8 @@ pokeApi.getPokemonsDetails = (pokemon) => {
         .then(convertPokemonApiDetailsToPokemon)
 };
 
-pokeApi.getPokemons = () => {
+pokeApi.getPokemons = (offset = 0, limit= 5) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
   return fetch(url)
     .then((response) => response.json())
     .then((responseBody) => responseBody.results)
